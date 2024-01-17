b90edecf22be47e451ce9e64bd3fe65073283ebe
zambiazzi <lucaszambiazzi194@gmail.com>
2024-01-17 10:14:28 -0300

    refatorando erros

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 
 // eslint-disable-next-line no-unused-vars
 export default function manipuladorDeErros(erro, req, res, next)  { // middleware de erro do express para uso em requisições
     if (erro instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos" });
+        new RequisicaoIncorreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
-        const mensagensErro = Object.values(erro.errors)
-            .map(erro => erro.message)
-            .join("; ");
-
-        res.status(400).send({ message: `Os seguintes erros foram encontrados: ${mensagensErro}` });
+        new ErroValidacao(erro).enviarResposta(res);
     } else {
-        res.status(500).send({ message: "Erro interno do servidor" });
+        new ErroBase().enviarResposta(res);
     }
 }
